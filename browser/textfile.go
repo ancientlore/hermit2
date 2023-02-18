d498112acd2c23a68cfa5324f81715542361fdbc
Michael Lore <michael.lore@gmail.com>
2023-02-18 15:03:24 -0500

    Try out syntax highlight

--- browser/textfile.go
+++ browser/textfile.go
@@ -1,9 +1,12 @@
 package browser
 
 import (
+	"bytes"
 	"io/ioutil"
 	"path/filepath"
 
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/quick"
 	"github.com/ancientlore/hermit2/scroller"
 	"github.com/ancientlore/hermit2/views"
 	tea "github.com/charmbracelet/bubbletea"
@@ -14,6 +17,17 @@ func NewTextModel(path string, prev tea.Model) tea.Model {
 	in, err := ioutil.ReadFile(filepath.FromSlash(path))
 	if err != nil {
 		in = []byte(err.Error())
+	}
+
+	var lexer string
+	if l := lexers.Match(path); l != nil {
+		lexer = l.Config().Name
+	}
+
+	var buf bytes.Buffer
+	err = quick.Highlight(&buf, string(in), lexer, "terminal256", "native")
+	if err == nil {
+		in = buf.Bytes()
 	}
 
 	return scroller.Model[views.Text]{
--- go.mod
+++ go.mod
@@ -8,8 +8,10 @@ require (
 )
 
 require (
+	github.com/alecthomas/chroma v0.10.0 // indirect
 	github.com/aymanbagabas/go-osc52 v1.2.1 // indirect
 	github.com/containerd/console v1.0.3 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
